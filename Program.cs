763361787935e4b41541d912b771be18265a6ac1
vn55d1z <vn55d1z@WEUS92315405667>
2023-01-28 14:26:44 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HolaMundo
+{
+    class Program{
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce tu nombre");
+            //Console.Readline() lee la siguiente linea en consola
+            String? nombre = Console.ReadLine();
+            Console.WriteLine("Bienvenido " + nombre);
+            Console.WriteLine("Ingrese la hora: ");
+            int? hora = int.Parse(Console.ReadLine());
+            Console.WriteLine("La hora ingresada es " + hora);
+        }
+    }
+}
+
